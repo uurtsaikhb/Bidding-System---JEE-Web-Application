70e9a81b2b072e10efa98aa30797a17125934965
Uurtsaikh Batbileg <uurtsaikh.ba@gmail.com>
2014-11-15 14:15:19 -0600

    First commit

---
+++ Desktop/Bingo/src/java/edu/mum/waa/models/Customer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mum.waa.models;
+
+/**
+ *
+ * @author uurtsaikh
+ */
+public class Customer {
+    
+    private String firstname;
+    private String lastname;
+    
+    
+}
