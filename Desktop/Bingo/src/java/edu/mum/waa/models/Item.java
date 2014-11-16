b1de62d95dfb15cc1d2c0fbd0fb086b73760b58b
Uurtsaikh Batbileg <uurtsaikh.ba@gmail.com>
2014-11-16 01:08:56 -0600

    Item model changed

--- Desktop/Bingo/src/java/edu/mum/waa/models/Item.java
+++ Desktop/Bingo/src/java/edu/mum/waa/models/Item.java
@@ -22,10 +22,64 @@ public class Item {
     private double startPrice;
     private double buyoutPrice;
     private double stepPrice;
+
+    public Item(int id, String name, String detail, double startPrice, double buyoutPrice, double stepPrice) {
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+        this.startPrice = startPrice;
+        this.buyoutPrice = buyoutPrice;
+        this.stepPrice = stepPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public double getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(double startPrice) {
+        this.startPrice = startPrice;
+    }
+
+    public double getBuyoutPrice() {
+        return buyoutPrice;
+    }
+
+    public void setBuyoutPrice(double buyoutPrice) {
+        this.buyoutPrice = buyoutPrice;
+    }
+
+    public double getStepPrice() {
+        return stepPrice;
+    }
+
+    public void setStepPrice(double stepPrice) {
+        this.stepPrice = stepPrice;
+    }
     
-    private List<Deal> bids = new ArrayList<Deal>();
-    private Category category;
-    private Customer owner;
     
     
 }
