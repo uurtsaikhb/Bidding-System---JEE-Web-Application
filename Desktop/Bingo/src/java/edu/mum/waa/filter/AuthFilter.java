d681466c86ffc1540069d6ec50b390c6c409c2ba
manda-ts <mandal@10.10.33.196>
2014-11-17 17:08:14 -0600

    Login creation started

---
+++ Desktop/Bingo/src/java/edu/mum/waa/filter/AuthFilter.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mum.waa.filter;
+
+import edu.mum.waa.controllers.LoginBean;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mandal
+ */
+@WebFilter(filterName = "AuthFilter", urlPatterns = {"*.xhtml"})
+public class AuthFilter implements Filter {
+
+    @Inject
+    private LoginBean loginBean;
+    
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        try {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
+            HttpSession session = req.getSession(false);
+
+            String reqURI = req.getRequestURI();
+            if (loginBean != null && !loginBean.isLoggedIn() && reqURI.indexOf("/login.xhtml") >= 0) {
+                System.out.println(loginBean.getUserName());
+                res.sendRedirect("index.xhtml");
+            }
+            chain.doFilter(request, response);
+        } catch (Throwable t) {
+            System.out.println(t.getMessage());
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
