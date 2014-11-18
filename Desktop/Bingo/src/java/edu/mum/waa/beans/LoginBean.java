ae20b8ef76c3bc0d30255b6da2f93cf0f195db87
manda-ts <mandal@10.10.33.196>
2014-11-17 19:50:33 -0600

    LoginBean change position

---
+++ Desktop/Bingo/src/java/edu/mum/waa/beans/LoginBean.java
@@ -0,0 +1,56 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mum.waa.beans;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author mandal
+ */
+@Named
+@SessionScoped
+public class LoginBean implements Serializable
+{
+    private String userName;
+    private String userPassword;
+    private boolean isLoggedIn;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+    
+    public String signIn()
+    {
+        isLoggedIn = true;
+        return "index.xhtml";
+    }
+    
+    public String signOut()
+    {
+        isLoggedIn = false;
+        return "login";
+    }
+
+    public boolean isLoggedIn() 
+    {
+        return isLoggedIn;
+    }
+}
