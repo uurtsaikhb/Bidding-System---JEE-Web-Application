0def7986b7d867ffc4216dd0644132ed6c5bb033
Javkhlant <javkhlant.ch@gmail.com>
2014-11-18 01:19:24 -0600

    - minor changes.

--- Desktop/Bingo/src/java/edu/mum/waa/beans/PictureBean.java
+++ Desktop/Bingo/src/java/edu/mum/waa/beans/PictureBean.java
@@ -5,9 +5,18 @@
  */
 package edu.mum.waa.beans;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Part;
+import org.primefaces.event.FileUploadEvent;
 
 /**
  *
@@ -16,11 +25,32 @@ import java.io.Serializable;
 @Named(value = "pictureBean")
 @SessionScoped
 public class PictureBean implements Serializable {
-
-    /**
-     * Creates a new instance of PictureBean
-     */
-    public PictureBean() {
-    }
     
+    private String destination = "/Users/javkhlant/Downloads/tmp/";
+
+    public void upload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void copyFile(String fileName, InputStream in) {
+        try {
+            OutputStream out = new FileOutputStream(new File(destination + fileName));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            in.close();
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
