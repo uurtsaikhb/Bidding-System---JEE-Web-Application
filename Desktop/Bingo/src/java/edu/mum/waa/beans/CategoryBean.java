b897533f57e8451ba5822bde8834922efc521e3c
manda-ts <mandal@10.10.33.196>
2014-11-18 00:15:42 -0600

    LoginBean change position

--- Desktop/Bingo/src/java/edu/mum/waa/beans/CategoryBean.java
+++ Desktop/Bingo/src/java/edu/mum/waa/beans/CategoryBean.java
@@ -6,27 +6,35 @@
 package edu.mum.waa.beans;
 
 import edu.mum.waa.controllers.CategoryFacadeLocal;
+import edu.mum.waa.models.Category;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 
 /**
  *
  * @author uurtsaikh
  */
 @Named(value = "categoryBean")
-@SessionScoped
+@RequestScoped
 public class CategoryBean implements Serializable {
 
     /**
      * Creates a new instance of CategoryBean
      */
-    
     @EJB
     CategoryFacadeLocal categoryController;
     
+    private List<Category> categories;
+    
     public CategoryBean() {
     }
+    
+    public List<Category>  getCategories (){
+        return categoryController.findAll();
+    }
+  
     
 }
